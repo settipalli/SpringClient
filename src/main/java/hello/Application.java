11fd64bf1302b8a0b45d0fadbaaaa13de831df98
Santhoshkumar Settipalli <santhosh.settipalli@gmail.com>
2019-02-24 22:37:20 +0000

    Fixed Application.java. Compiled the code using the command: `javac -cp ./src/main/java src/main/java/**/*.java -d ./target/classes`.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,5 +1,7 @@
 package hello;
 
 public class Application {
-    System.out.println("Hello Spring");
+    public static void main(String[] args) {
+        System.out.println("Hello Spring");
+    }
 }
